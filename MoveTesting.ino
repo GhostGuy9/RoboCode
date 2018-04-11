a1affcd279b368a0cf616fb99fba301f003c746a
Daniel Webster (Laptop) <webster.daniel692@gmail.com>
2018-04-11 10:29:46 -0700

    Uploaded code for Move Test on Thursday

--- MoveTesting.ino
+++ MoveTesting.ino
@@ -1,22 +1,67 @@
+#include <RedBotSoftwareSerial.h>
 #include <RedBot.h>
 
 RedBotMotors motors;
 
-const int buzzerPin = 9;
-
 void setup() 
 {
-  pinMode(buzzerPin, OUTPUT);
-  playNote(21, 200);
-  delay(500);
-  playNote(22, 200);
-  
-  motors.drive(-100);
 
 }
 
 void loop() 
 {
-  
+  //One Block move
+  //delay(1000);
+  //motors.leftMotor(200);
+  //motors.rightMotor(-200);
+  //delay(700);
+  //motors.drive(0);
 
+  //dorward 1
+  delay(1000);
+  motors.drive(-200);
+  delay(700*3);
+  motors.drive(0);
+
+  //turn 90 left
+  delay(1000);
+  motors.drive(-200);
+  delay(345);
+  motors.drive(0);
+
+  //forward 1
+  delay(1000);
+  motors.drive(-200);
+  delay(700);
+  motors.drive(0);
+    
+  //turn 90 right
+  delay(1000);
+  motors.drive(-200);
+  delay(345);
+  motors.drive(0);
+
+  //forward 3
+  delay(1000);
+  motors.drive(-200);
+  delay(700*3);
+  motors.drive(0);
+  
+  //turn 90 right
+  delay(1000);
+  motors.drive(-200);
+  delay(345*2);
+  motors.drive(0);
+
+  //forward 6
+  delay(1000);
+  motors.drive(-200);
+  delay(700*6);
+  motors.drive(0);
+
+  //turn 90 left
+  delay(1000);
+  motors.drive(-200);
+  delay(345*2);
+  motors.drive(0);
 }
