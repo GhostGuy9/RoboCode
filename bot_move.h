48379da6a9c52cc9aa3db10fc95b5a8963cfbe0f
Daniel Webster (Laptop) <webster.daniel692@gmail.com>
2018-04-17 11:41:43 -0700

    Added new Back code after Jada did this morning :P

--- bot_move.h
+++ bot_move.h
@@ -13,10 +13,11 @@ int motorR = motorL*0.945;
 //Forward Delay
 int forward = 750;
 
-void moveForward(double dis)
+void moveForward(double fdis)
 {
-  for(int go=0; go<=dis; go++)
+  for(int go=0; go<=fdis; go++)
   {
+    Serial.println("Move Forward Step: "+ go);
     delay(1000);
     motors.leftMotor(motorL);
     motors.rightMotor(0-motorR);
@@ -29,6 +30,7 @@ void turnRight(double turnR)
 {
   for(int go=0; go<=turnR; go++)
   {
+    Serial.println("Turn Right Step: "+ go);
     delay(1000);
     motors.leftMotor(motorL);
     motors.rightMotor(motorR);
@@ -41,10 +43,24 @@ void turnLeft(double turnL)
 {
   for(int go=0; go<=turnL; go++)
   {
+    Serial.println("Turn Left Step: "+ go);
     delay(1000);
     motors.leftMotor(0-motorL);
     motors.rightMotor(0-motorR);
     delay(turn);
+    motors.brake();
+  }
+}
+
+void moveBack(double bdis)
+{
+  for(int go=0; go<=bdis; go++)
+  {
+    Serial.println("Move Back Step: "+ go);
+    delay(1000);
+    motors.leftMotor(0-motorL);
+    motors.rightMotor(motorR);
+    delay(forward);
     motors.brake();
   }
 }
